07e52dbc43787e8df51268708c6a4a544b8fa336
Alexandru Safta <alexsafta98@gmail.com>
2023-06-16 13:58:49 +0300

    Added colors

---
+++ src/assets/theme/index.ts
@@ -0,0 +1,17 @@
+const Theme = {
+  red: "#FF0000",
+  lightRed: "#DC4A4A",
+  purple: "#892EFF",
+  blue: "#3690FA",
+  lightBlue: "#28C6D0",
+  yellow: "#FFBB0E",
+  green: "#72BD6C",
+  black: "#1A0D0D",
+  white: "#FFFFFF",
+  grey: "#858181",
+  lightGrey1: "#E8E8E8", // borderGrey
+  lightGrey2: "#EFEFEF", // loaderGrey
+  lightGrey3: "#FAFAFA", // inputBackgroundGrey
+}
+
+export default Theme;
