d2f8ea57c81991d62c434e7dea9cab2352fe6683
Alexandru Safta <alexsafta98@gmail.com>
2023-06-16 13:58:59 +0300

    Added icon component

---
+++ src/components/CustomIcons/CustomIcon.tsx
@@ -0,0 +1,42 @@
+
+import React from "react";
+import { AntDesign, Entypo, MaterialIcons } from "@expo/vector-icons";
+import Theme from "../../assets/theme";
+
+export enum IconsVariant {
+  AntDesign,
+  Entypo,
+  MaterialIcons
+}
+
+interface CustomIconProps {
+  variant: IconsVariant;
+  name:
+    | React.ComponentProps<typeof AntDesign>["name"]
+    | React.ComponentProps<typeof Entypo>["name"]
+    | React.ComponentProps<typeof MaterialIcons>["name"];
+  color?: string;
+  size?: number;
+  others?: object;
+}
+
+const CustomIcon = ({
+  variant,
+  name,
+  size = 16,
+  color = Theme.white,
+  others = {},
+}: CustomIconProps) => {
+  switch (variant) {
+    case IconsVariant.AntDesign:
+      return <AntDesign name={name} color={color} size={size} {...others} />;
+    case IconsVariant.Entypo:
+      return <Entypo name={name} color={color} size={size} {...others} />;
+    case IconsVariant.MaterialIcons:
+      return <MaterialIcons name={name} color={color} size={size} {...others} />;
+    default:
+      return null;
+  }
+};
+
+export default CustomIcon;
---
+++ src/components/CustomIcons/IconNames.ts
@@ -0,0 +1,18 @@
+export enum AntDesignIconsNames {
+  youtube = 'youtube',
+  link = 'link',
+  close = 'close',
+  closeCircle = 'closecircle',
+  like = 'like1',
+  views = 'eye',
+  directory = 'folderopen',
+  warning = 'exclamationcircle'
+}
+
+export enum EntypoIconsNames {
+  info = 'info-with-circle'
+}
+
+export enum MaterialIconsNames {
+  starCircle = 'stars'
+}
